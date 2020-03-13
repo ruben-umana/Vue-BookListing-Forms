eea65402c38ac6e20b031f73a7853a810febf9f8
Rubén Umaña <ruben.umana@gorillalogic.com>
2020-03-13 16:33:43 -0600

    cambio

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -2,6 +2,10 @@
   <form v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)" class="" action="#" method="post">
     <input v-model="bookTitle" type="text" name="title" value="" placeholder="Book Title">
     <input v-model="bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+    <div>
+      <input v-model="bookData.finishedReading" type="checkbox" name="finished" value="false">
+    </div>
+    
     <button type="submit" name="button">Add Book</button>
   </form>
 </template>
@@ -12,8 +16,12 @@ export default {
   props: ["books"],
   data() {
     return {
-      bookTitle: "",
-      bookAuthor: ""
+      bookData: {
+        bookTitle: "",
+        bookAuthor: "",
+        finishedReading: false,
+        ownership: []
+      }
     };
   },
   methods: {
